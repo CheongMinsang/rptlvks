1be52579e6a402ea0eb146b942514a4b70c832a0
CheongMinsang <lgu33993388@gmail.com>
2025-10-21 02:27:31 +0900

    테스트

---
+++ board/src/main/java/com/example/board/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+//パッケージが属しているという宣言
+package com.example.board.config;
+
+//Springが管理するオブジェクト
+import org.springframework.context.annotation.Bean;
+//設定するクラス
+import org.springframework.context.annotation.Configuration;
+//セキュリティ規則を設定するツール
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//Webセキュリティ機能活性化
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+//Spring設定ファイルというアノテーション
+@Configuration
+//Springセキュリティ機能をONにする
+@EnableWebSecurity
+
+
+public class SecurityConfig {
+    //Springが管理する
+    @Bean
+    //セキュリティ規則のチェイン
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        //http要請に対しての権限設定が始まる部分
+        http
+                .authorizeHttpRequests(auth -> auth
+                        //誰がどんなURLにアクセスできるのか
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
+
